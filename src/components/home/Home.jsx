25095d96a5a904d25b0b20a82929bb1d803bd3f6
RanKiro121 <rankiro121@gmail.com>
2024-08-04 21:45:46 +0530

    added about

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./home.css";
 import Social from "./Social";
 import Data from "./Data";
+import ScrollDown from "./ScrollDown";
 const Home = () => {
   return (
     <section className="home section" id="home">
@@ -12,6 +13,7 @@ const Home = () => {
 
           <Data />
         </div>
+        <ScrollDown />
       </div>
     </section>
   );
